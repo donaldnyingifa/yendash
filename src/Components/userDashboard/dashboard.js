aafe3067ad8ca42b1af3f1f26ada54834070c00f
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-09-30 18:02:36 +0100

    setup student

--- src/Components/userDashboard/dashboard.js
+++ src/Components/userDashboard/dashboard.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import { Container, Card } from 'react-bootstrap';
 import General from './General';
-
+import { Link } from 'react-router-dom'
+import Student from './Student'
 class dashboard extends Component {
     render() {
         return (
             <Container className='user-box'>
                 <div className='user-jumbo d-flex justify-content-center'>
                     <div className='user-text'>
-                    <h1 className='text-center'>“Knowledge is power, information is liberating.
+                        <h1 className='text-center'>“Knowledge is power, information is liberating.
 Education is the premise of progess in every socitey in every family”</h1>
-</div>
+                    </div>
 
                 </div>
                 <div className='d-flex flex-column mt-3'>
@@ -18,26 +19,20 @@ Education is the premise of progess in every socitey in every family”</h1>
                     <h5>What do you want to do today?</h5>
                 </div>
                 <div className='d-flex justify-content-between'>
-                    <Card>
+                    <Link to='/dashboard'><Card>
                         <Card.Body>
                             General Information
                         </Card.Body>
-                    </Card>
+                    </Card></Link>
 
-                    <Card>
+                    <Link to='/student'><Card>
                         <Card.Body>
-                           Teacher Details
+                            Student details
                         </Card.Body>
-                    </Card>
-
-                    <Card>
-                        <Card.Body>
-                           Student details
-                        </Card.Body>
-                    </Card>
+                    </Card></Link>
                 </div>
                 <div className='d-flex flex-column'>
-                    <General/>
+                    <General />
                 </div>
             </Container>
         )
