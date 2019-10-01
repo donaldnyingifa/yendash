150aa12977dfea87691deab1c700d6c8ab53981f
Donald Nyingifa <donaldnyingifa@gmail.com>
2019-10-01 02:03:56 +0100

    edited general

---
+++ build/precache-manifest.87737835baf65d5668a1912226ef0c01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d6e9be401067d15befe48f43b721ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e793e43206499535e96b",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9de3210a8587f4171a5b",
+    "url": "/static/css/main.cc01a9c3.chunk.css"
+  },
+  {
+    "revision": "e793e43206499535e96b",
+    "url": "/static/js/2.af7ba9eb.chunk.js"
+  },
+  {
+    "revision": "9de3210a8587f4171a5b",
+    "url": "/static/js/main.035f2898.chunk.js"
+  },
+  {
+    "revision": "629da966a84ce4f33309",
+    "url": "/static/js/runtime-main.bfc875e5.js"
+  },
+  {
+    "revision": "741b17ff6b9f29efda680cc2d5b17101",
+    "url": "/static/media/people.741b17ff.png"
+  },
+  {
+    "revision": "39755d3cc66a44fae083bb9ab7bff19b",
+    "url": "/static/media/pikin.39755d3c.jpg"
+  }
+]);
