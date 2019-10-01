f07ead775ccbc4f3f32b1216adac817b20b93042
Roland Enola <40168844+mystiexx@users.noreply.github.com>
2019-10-01 01:24:09 +0100

    new updates

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 import { Link } from 'react-router-dom'
+import logo from './school.png'
 import { auth } from '../../firebase'
 
 class Header extends Component {
@@ -28,19 +29,22 @@ class Header extends Component {
             <div>
                 <Container className="mt-3">
                     <div className='d-flex justify-content-around'>
-                        <Link to="/dashboard">
-                             <h1>Yendash</h1>
+                        <div className='d-flex flex-row'>
+                            <img src={logo} alt='logo' className='logo'/>
+                        <Link to="/dashboard" className='link'>
+                             <h1>SchoolPoint</h1>
                         </Link>
+                        </div>
                         <Form className='mt-2 form-input' >
                             <Form.Group>
                                 <Form.Control type='text' className='form-input' placeholder='Search' />
                             </Form.Group>
                         </Form>
 
-                        <Link to='/schools' className='mt-3'>Schools Registered</Link>
-                        <Link to='/new/schools' className='mt-3'>New Schools</Link>
+                        <Link to='/schools' className='mt-3 link'>Schools Registered</Link>
+                        <Link to='/new/schools' className='mt-3 link'>New Schools</Link>
 
-                        <a href="#" className='mt-3' onClick={this.logout}>
+                        <a href="#" className='mt-3 link' onClick={this.logout}>
                             Log Out
                         </a>
                     </div>
